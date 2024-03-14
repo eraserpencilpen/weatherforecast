00347b290bdfea87e4622a19ca97f60a462b3847
JennyEaint <123454321eaint@gmail.com>
2024-03-14 11:38:26 +0630

    Added get time function

--- weather/lib/home.dart
+++ weather/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,7 +10,20 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getTime();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
+}
+
+void getTime() async {
+  final response = await http.get(Uri.parse(
+      "https://timeapi.io/api/Time/current/coordinate?latitude=16.84&longitude=96.17"));
+  print(response.body);
 }
--- weather/lib/sth.dart
+++ weather/lib/sth.dart
@@ -6,14 +6,14 @@ import 'dart:io';
 import 'dart:convert';
 
 void main() async {
-  // final response = await http.get(Uri.parse(
-  //     "https://api.open-meteo.com/v1/forecast?latitude=16.819216322979972&longitude=96.15838214867654&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,weather_code"));
-  // print(response.body.runtimeType);
-  String path =
-      r"C:\Users\User\OneDrive\Desktop\Weather Forecast\weather\assets\wc_images";
-  weatherCodes.forEach((key, value) {
-    value["day"]["image"] = "$path\\wc_${key}_day.png";
-    value["night"]["image"] = "$path\\wc_${key}_night.png";
-  });
-  var myFile = File("file.json").writeAsString(jsonEncode(weatherCodes).toString());
+  final response = await http.get(Uri.parse(
+      "https://api.open-meteo.com/v1/forecast?latitude=16.819216322979972&longitude=96.15838214867654&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,weather_code"));
+  print(response.body.runtimeType);
+  getTime();
+}
+
+void getTime() async {
+  final response = await http.get(Uri.parse(
+      "https://timeapi.io/api/Time/current/coordinate?latitude=16.84&longitude=96.17"));
+  print(response.body);
 }
