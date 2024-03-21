064884a7cffe8aff840d05b9541846bce45cc156
JennyEaint <123454321eaint@gmail.com>
2024-03-21 11:57:39 +0630

    Minor changes

--- weather/lib/home.dart
+++ weather/lib/home.dart
@@ -85,28 +85,6 @@ class _WeatherWidgetState extends State<WeatherWidget> {
             double currentTime =
                 time["hour"].toDouble() * 60 + time["minute"].toDouble();
 
-            String sunrise_hour =
-                weatherData["daily"]["sunrise"][0].substring(11, 13);
-            String sunrise_minute =
-                weatherData["daily"]["sunrise"][0].substring(14, 16);
-            double sunrise =
-                double.parse(sunrise_hour) * 60 + double.parse(sunrise_minute);
-            String sunset_hour = weatherData["daily"]["sunrise"][0].substring(11, 13);
-            String sunset_minute = weatherData["daily"]["sunrise"][0].substring(14, 16);
-            print(sunrise);
-            print(currentTime);
-            // int sunriseMinute = int.parse(sunrise_minute);
-            // int sunrise = sunriseHour * sunriseMinute;
-            // var sunrise =
-            //     double.parse(sunrise_hour) * 60 + double.parse(sunrise_minute);
-            // print(sunrise.toString());
-            // print(double.parse(time["minute"]));
-            // double currentTime =
-            //     double.parse(time["hour"]) * 60 + double.parse(time["minute"]);
-            // var sunrise = weatherData["daily"]["sunrise"][0].substring(12, 16);
-            // var sunset = weatherData["daily"]["sunrise"].substring(12, 16);
-            // print(sunrise.toString());
-            // print(sunset.toString());
             return Row();
             // if (){
 
